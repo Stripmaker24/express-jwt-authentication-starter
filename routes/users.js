60e7a903ec3c3b07ab4181f1b48ac2121bac0b97
Stripmaker24 <33493759+Stripmaker24@users.noreply.github.com>
2022-06-05 16:03:51 +0200

    Setup and Register working
    Still need to implement the login function; will do that later tonight!

--- routes/users.js
+++ routes/users.js
@@ -9,9 +9,33 @@ router.get('/protected', (req, res, next) => {
 });
 
 // TODO
-router.post('/login', function(req, res, next){});
+router.post('/login', function(req, res, next){
+
+});
 
 // TODO
-router.post('/register', function(req, res, next){});
+router.post('/register', function(req, res, next){
+    const saltHash = utils.genPassword(req.body.password);
+
+    const salt = saltHash.salt;
+    const hash = saltHash.hash;
+
+    const newUser = new User({
+        username: req.body.username,
+        hash: hash,
+        salt: salt
+    });
+    
+    newUser.save()
+        .then((user) => {
+            const jwt = utils.issueJWT(user);
+            res.json({success: true, 
+                        user: user, 
+                        token: jwt.token, 
+                        expiresIn: jwt.expires});
+        })
+        .catch(err => next(err));
+
+});
 
 module.exports = router;
